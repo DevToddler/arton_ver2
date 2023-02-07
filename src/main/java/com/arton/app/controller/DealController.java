6319bb01609621175139a997e448d1c3b9806609
dev_toddler <99718527+DevToddler@users.noreply.github.com>
2023-02-08 07:36:05 +0900

    #arton_ver2

---
+++ src/main/java/com/arton/app/controller/DealController.java
@@ -0,0 +1,168 @@
+package com.arton.app.controller;
+
+import com.arton.app.domain.request.BookingRequest;
+import com.arton.app.service.DealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/deal")
+public class DealController {
+
+	@Autowired
+	DealService dealService;
+
+	@GetMapping("/deal")
+	public String dealForm(@RequestParam Integer perfCode, HttpSession session, Model m) {
+
+		/* 공연 상세 정보 */
+		m.addAttribute("perfDto", dealService.readPerf(perfCode));
+
+		/* 예매 섹션 - 회차별 좌석 및 잔여석 정보 */
+		m.addAttribute("seats", dealService.readSeatInfo(perfCode));
+
+		/* 사용자의 해당 공연 찜 여부 확인 */
+		Integer userIdx = (Integer) session.getAttribute("userIdx");
+		if (userIdx != null) {
+			m.addAttribute("isLiked", dealService.isLiked(perfCode, userIdx));
+		}
+
+		return "deal";
+	}
+
+
+	/* Ajax - 예매 */
+
+	@PostMapping("/book")
+	public @ResponseBody Map book(@RequestBody BookingRequest bookingRequest,
+								  HttpSession session,
+								  HttpServletResponse response,
+								  Model m) throws IOException {
+
+		Integer perfCode = bookingRequest.getPerfCode();
+		Integer userIdx = (Integer) session.getAttribute("userIdx");
+		bookingRequest.setUserIdx(userIdx);
+
+		/* 만약의 경우 로그인 없이 넘어왔을 경우 로그인 페이지로*/
+		if (userIdx == null) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('로그인이 필요합니다.'); location.href='/app/login/login'</script>");
+			out.flush();
+
+			return null;
+		}
+
+
+		/* 좌석 차감 */
+		int isBooked = dealService.minusSeatStock(bookingRequest);
+		int cnt = 99;
+
+		/* 좌석 차감 성공 시 예매데이터 삽입*/
+		if (isBooked == 1) {
+			cnt = dealService.makeBooking(bookingRequest); // cnt = 성공 시 1, 실패 시 0
+		} else if (isBooked == 0) {
+			cnt = 0;
+		}
+
+		Map map = new HashMap();
+		map.put("seatStocks", dealService.updateSeatStock(perfCode)); // 잔여석 갱신을 위한 List<ViewSeatInvt> 받아오기
+		map.put("result", cnt);
+
+		return map;
+	}
+
+	/* Ajax - 찜하기 */
+	@PostMapping("/like")
+	public String like(@RequestBody Map<String, String> map, HttpSession session, HttpServletResponse response) throws
+			Exception {
+
+		System.out.println(map);
+		System.out.println(map.get("perfCode"));
+		Integer userIdx = (Integer) session.getAttribute("userIdx");
+		System.out.println(1);
+		/* 만약의 경우 로그인 없이 넘어왔을 경우 로그인 페이지로*/
+		if (userIdx == null) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('로그인이 필요합니다.'); location.href='/app/login/login'</script>");
+			out.flush();
+			System.out.println(2);
+
+			return null;
+		}
+		System.out.println(3);
+
+		int perfCode = 0;
+		try {
+			perfCode = Integer.parseInt(map.get("perfCode"));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		System.out.println(perfCode);
+
+		boolean liked = dealService.isLiked(userIdx, perfCode);
+		System.out.println(liked);
+		System.out.println("0000000000000000");
+		if (liked) {
+			System.out.println("1");
+
+			dealService.offLike(userIdx, perfCode);
+		} else {
+			System.out.println("2");
+
+			dealService.onLike(userIdx, perfCode);
+		}
+		boolean bool = dealService.isLiked(userIdx, perfCode);
+
+		String str = "";
+		if (bool) {
+			str = "1";
+		} else {
+			str = "0";
+		}
+		return str;
+	}
+
+	//
+	//	@PostMapping("/deal")
+	//	public String review(@RequestBody ReviewDto reviewDto, HttpServletRequest request, Model m) {
+	//		Integer perfId = Integer.valueOf(request.getParameter("perfCode"));
+	//
+	//		HttpSession session = request.getSession();
+	//		Integer userIdx = (Integer) session.getAttribute("userIdx");
+	//
+	//		// 리뷰 전체 가져오기
+	//		List<ReviewDto> review = dealService.readList(perfId);
+	//		m.addAttribute("review", review);
+	//		System.out.println("review : " + review);
+	//
+	//		// 리뷰 작성
+	//		reviewDto.setUserIdx(userIdx);
+	//		reviewDto.setPerfId(perfId);
+	//		dealService.write(reviewDto);
+	//
+	//		// 리뷰 수정
+	//		dealService.modify(reviewDto);
+	//
+	//		// 리뷰 삭제
+	//		String reviewId = request.getParameter("reviewId");
+	//		Map map = new HashMap();
+	//		map.put("reviewId", reviewId);
+	//		map.put("userIdx", userIdx);
+	//		dealService.remove(map);
+	//
+	//		return "deal";
+	//	}
+
+
+}
